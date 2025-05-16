5a8efcbea310e150e71c387153ffe8df520c4857
vitor <vitalispwe@gmail.com>
2025-05-16 20:35:34 +0100

    Lighting meeting - Commit check

---
+++ Project/FleetManager/headers/model/date.h
@@ -0,0 +1,22 @@
+//
+// Created by vitor on 5/16/25.
+//
+
+#ifndef DATE_H
+#define DATE_H
+
+
+
+class date {
+    private:
+      int day;
+      int month;
+      int year;
+
+    public:
+
+};
+
+
+
+#endif //DATE_H
---
+++ Project/FleetManager/headers/model/driver.h
@@ -0,0 +1,20 @@
+//
+// Created by vitor on 5/16/25.
+//
+
+#ifndef DRIVER_H
+#define DRIVER_H
+
+
+
+class date {
+private:
+
+
+public:
+
+};
+
+
+
+#endif //DRIVER_H
