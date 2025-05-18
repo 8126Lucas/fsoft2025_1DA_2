13cb2280b7f666f70a9e4fe3d67e18826abfbf6f
lucas <lucaspereirasantos8126@gmail.com>
2025-05-18 20:57:58 +0100

    Começo da separação entre Vehicle.cpp/.h e VehicleView.cpp/.h.

---
+++ Project/FleetManager/headers/views/VehicleView.h
@@ -0,0 +1,18 @@
+//
+// Created by lucas on 18/05/2025.
+//
+
+#ifndef HEADERS_VIEWS_VEHICLEVIEW_H
+#define HEADERS_VIEWS_VEHICLEVIEW_H
+#include <list>
+#include "Student.h"
+
+class StudentView {
+public:
+    void addVehicle();
+    void removeVehicle();
+    void listVehicles();
+    void listAvailableVehicles();
+};
+
+#endif //HEADERS_VIEWS_VEHICLEVIEW_H
