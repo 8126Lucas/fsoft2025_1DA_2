7c21f3112a33af8f20bc4f0337ef10ad46cf38e9
Ruben27Silva <rubenfilipecarvalhosilva24@gmail.com>
2025-05-19 10:37:36 +0100

    Corrigir Date.h Header

--- Project/FleetManager/headers/model/Date.h
+++ Project/FleetManager/headers/model/Date.h
@@ -2,23 +2,24 @@
 // Created by vitor on 5/16/25.
 //
 
-#ifndef HEADERS_MODEL_DATE_H_
-#define HEADERS_MODEL_DATE_H_
+#ifndef HEADERS_MODEL_DATE_H
+#define HEADERS_MODEL_DATE_H
 
 
 
 class Date {
-    private:
+private:
       int day;
       int month;
       int year;
       bool isLeapYear(int year);
       bool isValid(int day, int month, int year);
-};
 
-    public:
+public:
       Date();
       void setDate(int day, int month, int year);
       void getDate(int& day, int& month, int& year);
+};
+
 
 #endif //HEADERS_MODEL_DATE_H
