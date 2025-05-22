ec68bfd5de5354cf9028cc6715e550e40fdf0105
Ruben27Silva <rubenfilipecarvalhosilva24@gmail.com>
2025-05-22 21:57:54 +0100

    Implementação da maioria dos métodos restantes no Driver.cpp. Adição dos métodos restantes ao Driver.h, Expense.h e Revenue.h. Pequenas correções no FinancialContainer.h e no Vacation.h.

---
+++ Project/FleetManager/cmake-build-debug/CMakeFiles/FleetManager.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/main.cpp" "CMakeFiles/FleetManager.dir/main.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/main.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/controllers/Controller.cpp" "CMakeFiles/FleetManager.dir/sources/controllers/Controller.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/controllers/Controller.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/exceptions/DuplicatedDataException.cpp" "CMakeFiles/FleetManager.dir/sources/exceptions/DuplicatedDataException.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/exceptions/DuplicatedDataException.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/exceptions/InvalidDataException.cpp" "CMakeFiles/FleetManager.dir/sources/exceptions/InvalidDataException.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/exceptions/InvalidDataException.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/exceptions/NonExistingDataException.cpp" "CMakeFiles/FleetManager.dir/sources/exceptions/NonExistingDataException.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/exceptions/NonExistingDataException.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Date.cpp" "CMakeFiles/FleetManager.dir/sources/model/Date.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Date.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Driver.cpp" "CMakeFiles/FleetManager.dir/sources/model/Driver.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Driver.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/DriverContainer.cpp" "CMakeFiles/FleetManager.dir/sources/model/DriverContainer.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/DriverContainer.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Enterprise.cpp" "CMakeFiles/FleetManager.dir/sources/model/Enterprise.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Enterprise.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Expense.cpp" "CMakeFiles/FleetManager.dir/sources/model/Expense.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Expense.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Inspection.cpp" "CMakeFiles/FleetManager.dir/sources/model/Inspection.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Inspection.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Insurance.cpp" "CMakeFiles/FleetManager.dir/sources/model/Insurance.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Insurance.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Order.cpp" "CMakeFiles/FleetManager.dir/sources/model/Order.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Order.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/OrderContainer.cpp" "CMakeFiles/FleetManager.dir/sources/model/OrderContainer.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/OrderContainer.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Trip.cpp" "CMakeFiles/FleetManager.dir/sources/model/Trip.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Trip.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/TripContainer.cpp" "CMakeFiles/FleetManager.dir/sources/model/TripContainer.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/TripContainer.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Truck.cpp" "CMakeFiles/FleetManager.dir/sources/model/Truck.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Truck.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/VSLContainer.cpp" "CMakeFiles/FleetManager.dir/sources/model/VSLContainer.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/VSLContainer.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Van.cpp" "CMakeFiles/FleetManager.dir/sources/model/Van.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Van.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/Vehicle.cpp" "CMakeFiles/FleetManager.dir/sources/model/Vehicle.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/Vehicle.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/VehicleContainer.cpp" "CMakeFiles/FleetManager.dir/sources/model/VehicleContainer.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/VehicleContainer.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/model/VehicleStorageLocation.cpp" "CMakeFiles/FleetManager.dir/sources/model/VehicleStorageLocation.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/model/VehicleStorageLocation.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/views/InsuranceView.cpp" "CMakeFiles/FleetManager.dir/sources/views/InsuranceView.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/views/InsuranceView.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/views/Utils.cpp" "CMakeFiles/FleetManager.dir/sources/views/Utils.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/views/Utils.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/views/VehicleStorageLocationView.cpp" "CMakeFiles/FleetManager.dir/sources/views/VehicleStorageLocationView.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/views/VehicleStorageLocationView.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/views/VehicleView.cpp" "CMakeFiles/FleetManager.dir/sources/views/VehicleView.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/views/VehicleView.cpp.obj.d"
+  "C:/Projects/fsoft2025_1DA_2/Project/FleetManager/sources/views/View.cpp" "CMakeFiles/FleetManager.dir/sources/views/View.cpp.obj" "gcc" "CMakeFiles/FleetManager.dir/sources/views/View.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
