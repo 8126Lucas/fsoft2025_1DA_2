62fa722124fba2e98dd9b665e36b06e5df2b2a62
lucas <lucaspereirasantos8126@gmail.com>
2025-05-24 18:45:38 +0100

    Começo do Controller.cpp/.h. Controller() e run().

--- Project/FleetManager/headers/controllers/Controller.h
+++ Project/FleetManager/headers/controllers/Controller.h
@@ -4,5 +4,37 @@
 
 #ifndef HEADERS_CONTROLLERS_CONTROLLER_H
 #define HEADERS_CONTROLLERS_CONTROLLER_H
+#include "DriverView.h"
+#include "Enterprise.h"
+#include "FinancialView.h"
+#include "InspectionView.h"
+#include "InsuranceView.h"
+#include "OrderView.h"
+#include "TripView.h"
+#include "VehicleView.h"
+#include "View.h"
+
+class Controller {
+private:
+    Enterprise model;
+    View view;
+    DriverView driverView;
+    FinancialView financialView;
+    InspectionView inspectionView;
+    InsuranceView insuranceView;
+    OrderView orderView;
+    TripView tripView;
+    VehicleView vehicleView;
+    void runDriver();
+    void runFinancial();
+    void runOrder();
+    void runTrip();
+    void runVehicle();
+
+public:
+    Controller(Enterprise &enterprise);
+    void run();
+};
+
 
 #endif //HEADERS_CONTROLLERS_CONTROLLER_H
--- Project/FleetManager/sources/controllers/Controller.cpp
+++ Project/FleetManager/sources/controllers/Controller.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "Controller.h"
+#include <iostream>
+#include "Utils.h"
+
+using namespace std;
+
+Controller::Controller(Enterprise &enterprise) {
+    this->model = enterprise;
+}
+
+void Controller::run() {
+    int op = -1;
+    do {
+        op = this->view.menuEnterprise();
+        switch (op) {
+            case 1:
+                runVehicle();
+                break;
+            case 2:
+                runDriver();
+                break;
+            case 3:
+                runTrip();
+                break;
+            case 4:
+                runFinancial();
+                break;
+            case 5:
+                runOrder();
+                break;
+            default:
+                break;
+        }
+    } while (op != 0);
+}
