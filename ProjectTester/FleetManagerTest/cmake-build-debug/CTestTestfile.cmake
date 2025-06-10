06b77532a4f83242b8db57caa0333ad7d5d96e74
VitorTeixeira <1232067@isep.ipp.pt>
2025-06-10 23:03:50 +0100

    Corrigido erro na implementação da Framework de testes. Corrigidos alguns erros nos testes do date_test.cpp. Existem 3 testes a falhar e 12 a passar.

---
+++ ProjectTester/FleetManagerTest/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vitorteixeira/Desktop/FSOFT/PL3/fsoft2025_1DA_2/ProjectTester/FleetManagerTest
+# Build directory: /Users/vitorteixeira/Desktop/FSOFT/PL3/fsoft2025_1DA_2/ProjectTester/FleetManagerTest/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/vitorteixeira/Desktop/FSOFT/PL3/fsoft2025_1DA_2/ProjectTester/FleetManagerTest/cmake-build-debug/runTests[1]_include.cmake")
+subdirs("googleTestLib/googletest-main")
